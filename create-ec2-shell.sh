d1e8a9c7d3aaaa079dedd11f0056637a13e8cfd1
nikhil <anc@gmail.com>
2023-09-02 11:11:20 +0530

    all

--- create-ec2-shell.sh
+++ create-ec2-shell.sh
@@ -16,10 +16,10 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z10334032ZMZZ509Q21DU
+    aws route53 change-resource-record-sets --hosted-zone-id Z10334032ZMZZ509Q21DU --change-batch '
 
     {
-        
+
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
@@ -29,4 +29,5 @@ do
                                  "ResourceRecords": [{ "Value": "$IP_ADDRESS"}]
                         }}]
     }
-    done
+    '
+done
